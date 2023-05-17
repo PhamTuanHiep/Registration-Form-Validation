ade82748284c0176b4dcbf0aaabed3d2934402a9
Tuan Hiep <tuanhiep@gmail.com>
2023-05-17 13:30:32 +0700

    popup + fixbug for validation

--- assets/js/Validate.js
+++ assets/js/Validate.js
@@ -1,79 +1,78 @@
 const btnSubmit = document.querySelector("#form-submit");
+const popup = document.getElementById("popup");
 
-const userName = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const passwordConfirm = document.getElementById("password_confirm");
-
-let userNameValue = "";
-let emailValue = "";
-let passwordValue = "";
-let passwordConfirmValue = "";
-
-let validUserName = false;
-let validEmail = false;
-let validEPass = false;
-let validPassConfirm = false;
 var isValid = false;
 
-userName.onchange = function () {
-  userNameValue = userName.value;
-  if (userNameValue == "") {
-    setError(userName, "User Name cannot be blank");
-    validUserName = false;
-  } else if (!isUserName(userNameValue)) {
-    setError(userName, "Invalid UserName");
-    validUserName = false;
-  } else {
-    setSuccess(userName);
-    validUserName = true;
-  }
-  validate();
+var input = [
+  (inputUserName = {
+    element: document.getElementById("username"),
+    message1: "User Name cannot be blank",
+    message2: "Invalid UserName",
+    isValid: false,
+    Valid: function (value) {
+      regex =
+        /^([a-zỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ']+)((\s{1}[a-zỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ']+)*$)/gi;
+      return regex.test(value);
+    },
+  }),
+  (inputEmail = {
+    element: document.getElementById("email"),
+    message1: "Email cannot be blank",
+    message2: "Invalid email",
+    isValid: false,
+    Valid: function (value) {
+      let regex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(value);
+    },
+  }),
+  (inputPass = {
+    element: document.getElementById("password"),
+    message1: "password cannot be blank",
+    message2: "Invalid password",
+    isValid: false,
+    Valid: function (value) {
+      let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,32}$/g;
+      let passConfValue = input[3].element.value || "";
+      if (passConfValue != "") {
+        Validation(input[3]);
+        if (passConfValue !== value) {
+          this.message2 = "password and password verification do not match";
+        }
+        return regex.test(value) && passConfValue === value;
+      } else {
+        return regex.test(value);
+      }
+    },
+  }),
+  (inputPassConfirm = {
+    element: document.getElementById("password_confirm"),
+    message1: "password confirm cannot be blank",
+    message2: "password and password verification do not match",
+    isValid: false,
+    Valid: function (value) {
+      console.log(input[2].element.value);
+      return value === input[2].element.value;
+    },
+  }),
+];
+input[0].element.onchange = function () {
+  Validation(input[0]);
+  isValidation();
+};
+input[1].element.onchange = function () {
+  Validation(input[1]);
+  isValidation();
+};
+input[2].element.onchange = function () {
+  Validation(input[2]);
+  isValidation();
 };
 
-email.onchange = function () {
-  emailValue = email.value;
-  if (emailValue == "") {
-    setError(email, "Email cannot be blank");
-    validEmail = false;
-  } else if (!isEmail(emailValue)) {
-    setError(email, "Invalid email");
-    validEmail = false;
-  } else {
-    setSuccess(email);
-    validEmail = true;
-  }
-  validate();
-};
-
-password.onchange = function () {
-  passwordValue = password.value;
-  if (passwordValue == "") {
-    setError(password, "password cannot be blank");
-    validEPass = false;
-  } else if (!isPassword(passwordValue)) {
-    setError(password, "Invalid password");
-    validEPass = false;
-  } else {
-    setSuccess(password);
-    validEPass = true;
-  }
-  validate();
-};
-
-passwordConfirm.onchange = function () {
-  passwordConfirmValue = passwordConfirm.value;
-  if (passwordConfirmValue == "") {
-    setError(passwordConfirm, "password confirm cannot be blank");
-    validPassConfirm = false;
-  } else if (!(passwordConfirmValue === passwordValue)) {
-    setError(passwordConfirm, "Invalid password confirm");
-    validPassConfirm = false;
-  } else {
-    setSuccess(passwordConfirm);
-    validPassConfirm = true;
-  }
-  validate();
+input[3].element.onchange = function () {
+  Validation(input[3]);
+  Validation(input[2]);
+  isValidation();
 };
 
 function getParent(element, selector) {
@@ -98,35 +97,44 @@ function setSuccess(ele) {
   parentEle.classList.add("success");
   parentEle.querySelector(".form-message").innerText = "";
 }
+function Validation(obj) {
+  if (obj.element.value == "") {
+    setError(obj.element, obj.message1);
+    obj.isValid = false;
+  } else if (!obj.Valid(obj.element.value)) {
+    setError(obj.element, obj.message2);
+    obj.isValid = false;
+  } else {
+    setSuccess(obj.element);
+    obj.isValid = true;
+  }
+  isValidation();
+}
 
-function isUserName(value) {
-  let regex = /\b[a-zA-Z]+\b$/gm;
-  return regex.test(value);
-}
-function isEmail(value) {
-  let regex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(value);
-}
-function isPassword(value) {
-  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,32}$/g;
-  return regex.test(value);
-}
-function validate() {
+function isValidation() {
+  let validUserName = input[0].isValid;
+  let validEmail = input[1].isValid;
+  let validEPass = input[2].isValid;
+  let validPassConfirm = input[3].isValid;
+
   if (validUserName && validEmail && validEPass && validPassConfirm) {
     isValid = true;
     btnSubmit.disabled = false;
     btnSubmit.classList.remove("disabled");
   } else {
+    isValid = false;
     btnSubmit.disabled = true;
+    popup.classList.remove("show");
     btnSubmit.classList.add("disabled");
   }
-  console.log("hello");
 }
-console.log("hi");
+
 btnSubmit.onclick = function (e) {
   e.preventDefault();
   if (isValid) {
-    alert("Register success !");
+    popup.classList.add("show");
+  } else {
+    btnSubmit.disabled = true;
+    btnSubmit.classList.add("disabled");
   }
 };
